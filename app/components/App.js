ce6960c32069c1bf194e585259435bcd53969b20
tgcoding <godfrey.tom.1@gmail.com>
2020-12-15 21:31:34 -0500

    Submit history (#1)
    * Initial meal submission page
    
    * New submission button hovering top right
    
    * MealHistory number and date formatting
    
    * MealHistory linting fixes

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Navigation from "./Navigation";
 import MealCalendar from "./MealCalendar";
+import MealHistory from "./MealHistory";
 
 class App extends Component {
     constructor(props) {
@@ -14,10 +15,18 @@ class App extends Component {
                 <div>
                     <Navigation />
                     <div className="container mt-5">
-                        <h1>Meals Calendar</h1>
+                        <div className="col-md-12">
+                            <h1 className="d-inline">Meals Calendar</h1>
+                            <div className="float-right">
+                                <Link to="/history"><button className="btn btn-primary ml-2">New</button></Link>
+                            </div>
+                        </div>
                         <Route exact={true} path="/" render={() => (
                             <div className="row mt-5">
                                 <div className="col-md-12">
+                                    <div className="row">
+                                        <Link to="/history">New Meal</Link>
+                                    </div>
                                     <div className="row">
                                         <Link to="/calendar">Calendar</Link>
                                     </div>
@@ -26,6 +35,7 @@ class App extends Component {
                         )}
                         />
                     </div>
+                    <Route path="/history" component={MealHistory} />
                     <Route path="/calendar" component={MealCalendar} />
                 </div>
             </Router>
